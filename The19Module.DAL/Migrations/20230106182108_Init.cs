a2de1abfb0ae069c1902843efce80e6d058bb786
- <wavevape@gmail.com>
2023-01-06 21:23:24 +0300

    Добавлены миграции

---
+++ The19Module.DAL/Migrations/20230106182108_Init.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace The19Module.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.CreateTable(
+            //    name: "Person",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(type: "int", nullable: false)
+            //            .Annotation("SqlServer:Identity", "1, 1"),
+            //        Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+            //        SecondName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+            //        Patronymic = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+            //        PhoneNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+            //        Adress = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+            //        Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+            //    },
+            //    constraints: table =>
+            //    {
+            //    });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropTable(
+            //    name: "Person");
+        }
+    }
+}
