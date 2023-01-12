70b3a52855765e970c3305501dd0368c63dc538a
- <wavevape@gmail.com>
2023-01-12 22:04:17 +0300

    Были удалены контроллеры второй страницы и вывода по Id. Вся логика была перенесена в PersonController и расписана с помощью #region. Были добавлены две базовые вью - успешного и безуспешного действия. Так же сразу были нарисованы легкие шаблоны.
    Глобально - приложение готово. Есть много работы по некоторым визуальным составляющим. Например сделать главную страницу, чуть красивее сделать общую таблицу, может добавить кнопки действий или даже перести функционал редактирования и удаления уже в главную карточку клиента.
    
    Варанты есть. Под капотом, скорее всего - 99% реализации сделано.

--- The_19_Module/Controllers/PersonController.cs
+++ The_19_Module/Controllers/PersonController.cs
@@ -1,4 +1,5 @@
 ﻿using _19Module.Domain.Interfaces;
+using _19Module.Domain.PersonClasses;
 using _19Module.Domain.Responce;
 using _19Module.Domain.ViewModels;
 using _The19Module.Services.Interfaces;
@@ -11,20 +12,51 @@ namespace The_19_Module.Controllers
     public class PersonController : Controller
     {
 
+        #region Private поля и свойства
+
         private readonly IPersonService _personService;
         private readonly IPersonViewService _personViewService;
+
+        private const string _pathToBadView = "/Views/Person/PatternsViews/BaseError.cshtml";
+        private const string _pathTSuccessView = "/Views/Person/PatternsViews/SuccessfullView.cshtml";
         public PersonController( IPersonService personService, IPersonViewService personViewService)
         {
             _personViewService = personViewService;
             _personService = personService;
         }
 
-       
-        public IActionResult Index()
+        #endregion
+
+        #region Вывод таблицы всех клиентов на экран
+        [HttpGet]
+        public IActionResult ShowAllPersons()
         {
-            return View();
+            var result = _personService.GetAllPersons();
+
+            if (result.CodeError == _19Module.Domain.Enums.StatusCode.SomeError)
+            {
+                return RedirectToAction("_pathToBadView");
+            }
+
+            return View(result);
         }
 
+        #endregion
+
+        #region Реализация показа полного досье
+        public IActionResult ShowPersonById(int id)
+        {
+            IBaseResponce<Person> result = _personService.GetPersonById(id);
+
+            if (result.CodeError == _19Module.Domain.Enums.StatusCode.PersonNotFound)
+            {
+                return View(_pathToBadView, result.Description);
+            }
+
+            return View(result);
+        }
+
+        #endregion
 
         #region Реализация добавления нового клиента
 
@@ -52,19 +84,29 @@ namespace The_19_Module.Controllers
 
         #region Реализация редактирования старого клиента
 
+        /// <summary>
+        /// Здесь получаем Id и переводим клиента во вью-класс
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet]
-        public IActionResult GetPersonById(int id)
+        public IActionResult EditPersonById(int id)
         {
             var personViewMode =  _personViewService.GetViewPersonById(id);
 
             if (personViewMode.CodeError == _19Module.Domain.Enums.StatusCode.PersonNotFound)
             {
-                return View("PersonDidntFound");
+                return View(_pathToBadView, "Клиент не был найден");
             }
 
             return View("EditPerson", personViewMode.Data);
         }
 
+        /// <summary>
+        /// Здесь уже непосредственный запрос к базе данных и изменение клиента
+        /// </summary>
+        /// <param name="personViewModel"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult EditPerson(PersonViewModel personViewModel)
         {
@@ -74,7 +116,7 @@ namespace The_19_Module.Controllers
 
                 if (result.Data is true)
                 {
-                    return View("ICouldEditPerson");
+                    return View(_pathTSuccessView);
                 }  
             }
 
@@ -86,7 +128,7 @@ namespace The_19_Module.Controllers
 
         #region Реализация удаления имеющегося клиента
 
-        [HttpPost]
+        
         public IActionResult DeletePerson(int id)
         {
             IBaseResponce<bool> response = _personService.Delete(id);
@@ -96,10 +138,10 @@ namespace The_19_Module.Controllers
                 string answer = $"Удаление прошло успешно";
                                 
 
-                return View("SuccessfullView", answer);
+                return View(_pathTSuccessView, answer);
             }
 
-            return View("BaseError", response.Data);
+            return View(_pathToBadView);
         }
 
         #endregion
